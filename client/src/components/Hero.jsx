123a98c133bafbe2d5340578c70168562ea4b859
Abhinav09062005 <abhinavanandhcs@gmail.com>
2025-10-02 14:29:50 +0530

    hero section

---
+++ client/src/components/Hero.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+const Hero = () => {
+  return (
+    <div>
+        <div>
+            <h2>Over 10,000+ jobs to apply</h2>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio perferendis at, debitis blanditiis non consequuntur incidunt ratione? Provident!</p>
+        <div>
+            <div>
+                <img src={assets.search_icon} alt="" />
+            </div>
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
-
+import Hero from '../components/Hero'
 const home = () => {
   return (
     <div>
       <Navbar/>
+      <Hero/>
     </div>
   )
 }
