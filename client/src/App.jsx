85c27d35f8ecd70cea87225223982425a93ddc42
Abhinav09062005 <abhinavanandhcs@gmail.com>
2025-10-22 18:49:02 +0530

    add pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,10 @@ import Applications from './pages/Applications'
 import ApplyJob from './pages/ApplyJob'
 import RecruiterLogin from './components/RecruiterLogin'
 import { AppContext } from './context/AppContext'
+import Dashboard from './pages/Dashboard'
+import AddJob from './pages/AddJob'
+import ManageJobs from './pages/ManageJobs'
+import ViewApplication from './pages/ViewApplications'
 const App = () => {
   const {showRecruiterLogin}=React.useContext(AppContext)
   return (
@@ -13,8 +17,15 @@ const App = () => {
       {showRecruiterLogin && <RecruiterLogin/>}
       <Routes>
         <Route path='/' element={<Home/>}/>
-        <Route path='apply-job/:id' element={<ApplyJob/>}/>
-        <Route path='applications' element={<Applications/>}/>
+        <Route path='/apply-job/:id' element={<ApplyJob/>}/>
+        <Route path='/applications' element={<Applications/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}>
+        <Route path='add-job' element={<AddJob/>}/>
+        <Route path='manage-job' element={<ManageJobs/>}/>
+        <Route path='view-applications' element={<ViewApplication/>}/>
+     
+        </Route>
+
 
       </Routes>
     </div>
